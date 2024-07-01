9a050d03406ba8663af2cc01ff48bb526f65b929
Fo-Zi <facundozingarelli@gmail.com>
2024-07-01 13:03:45 -0300

    Code snippets: Request and response

--- code_snippets/C_sharp/json_requests.cs
+++ code_snippets/C_sharp/json_requests.cs
@@ -1,8 +1,20 @@
+// ======================================================================================================== //
+// This code was ran under the following characteristics:
+// - Compiler: NET Core 3.1
+// - NuGet Packages:
+//      - System.Text.Json
+// ======================================================================================================== //
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+					
+// ======================================================================================================== //
 // Since the Reader acts as an API through some network interface and provides high-level commands to interact
 // either with the Reader itself, with the RFID module, or with the sensors; all through JSON based commands,
 // then it's recommended to create helper classes that allow you to convert from/to JSON's. Basing ourselves
 // on the showned Json command structure:
-// ------------------>>>>>>>>>>>>>>>>>>
+// ======================================================================================================== //
 public class GeneralCommandStructure {
 
         [JsonPropertyName("object")]
@@ -15,33 +27,45 @@ public class GeneralCommandStructure {
         public object Data { get; set; }
 
         [JsonPropertyName("status")]
-        public string Status { get; set; } = null; 
+        public string Status { get; set; }
+        
+        public GeneralCommandStructure(string obj, string target, string action , object data){
 
-}
-
-// Then creating the command is as simple as:
-public static object ConstructGeneralJson(string obj, string target, string action , object data) {
+			var arg = new Dictionary<string, object>() { { "target" , target }, { "action" , action } };
             
-        var arg = new Dictionary<string, object>() { { target , action } };
+            this.Object = obj;
+            this.Arg = arg;
+            this.Data = data;
+            this.Status = null;
 
-        var json = new GeneralCommandStructure
-        {
-            Object = obj,
-            Arg = arg ,
-            Data = data
-        };
-
-        return json;
-
+        }
+	
 }
 
+// ======================================================================================================== //
 // Somwhere in the code ->
-{       
+public class Program
+{
+    public static void Main()
+    {
         // Constructing the command ->
         var data = new Dictionary<string, object>() { { "tag_type", "generic" } };
-        var json = new GeneralCommandStructure{ "sensors" , "epc_and_tid" , "get" , data , null };
+        var command_obj = new GeneralCommandStructure("sensors", "epc_and_tid", "get", data);
 
-        // Making request and program logic ->
-        // ###################################
+        try{
+            
+            // Serializing the command to JSON string ->
+            string commandJsonString = JsonSerializer.Serialize(command_obj, new JsonSerializerOptions { WriteIndented = true });
+
+            // Printing the JSON string ->
+            Console.WriteLine(commandJsonString);
+
+        }
+        catch (JsonException ex)
+        {
+            Console.WriteLine("JsonException occurred: " + ex.Message);
+        }
+
+    }
 
 }
